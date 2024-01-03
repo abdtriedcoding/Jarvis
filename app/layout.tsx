3705da13f2e58bc4f111c46b07053488900ba1a4
sidd-abdullah <siddabdullahofficial@gmail.com>
2024-01-03 18:22:50 +0530

    fix ui bugs

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,14 +20,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} flex h-full`}>
-        <aside className="w-1/5 p-4 bg-slate-50">
+        <aside className="w-64 p-4 hidden md:block bg-slate-50">
           <Sidebar />
         </aside>
-        <main className="w-4/5 overflow-y-auto scrollbar-thin scrollbar-thumb-slate-500">
+        <main className="flex-1 overflow-y-auto scrollbar-thin scrollbar-thumb-slate-300">
           <Navbar />
-          <main className="max-w-4xl mx-auto">
-          {children}
-          </main>
+          <main className="max-w-3xl mx-auto p-4">{children}</main>
         </main>
       </body>
     </html>
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -6,30 +6,34 @@ import ProfileDropdown from "./profile-dropdown";
 
 const Sidebar = () => {
   return (
-    <div className="space-y-6">
-      {/* New chat button */}
-      <Link href={"/"}>
-        <div className="flex justify-between items-center text-center bg-gray-100 hover:bg-gray-200 p-1 rounded-md">
-          <div className="flex space-x-2 items-center text-center">
-            {/* Avatar */}
-            <Avatar>
-              <AvatarImage src="https://github.com/shadcn.png" />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-            {/* New chat */}
-            <h3 className="text-md font-medium">New chat</h3>
+    <>
+      <div className="flex flex-col h-full space-y-4">
+        {/* New chat */}
+        <Link href={"/"}>
+          <div className="flex justify-between items-center text-center bg-gray-100 hover:bg-gray-200 p-1 rounded-md">
+            <div className="flex space-x-2 items-center text-center">
+              {/* Avatar */}
+              <Avatar>
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+              {/* New chat */}
+              <h3 className="text-md font-medium">New chat</h3>
+            </div>
+            {/* Pencil Button */}
+            <PenSquareIcon className="w-5 h-5" />
           </div>
-          {/* Pencil Button */}
-          <PenSquareIcon className="w-5 h-5" />
+        </Link>
+
+        {/* Chat history */}
+        <div className="max-h-[480px] pr-2 overflow-y-auto scrollbar-thin scrollbar-thumb-gray-200">
+          <ChatHistoryBox />
         </div>
-      </Link>
-      {/* Chat history */}
-      <div className="max-h-[480px] pr-2 overflow-y-auto scrollbar-thin scrollbar-thumb-gray-200">
-        <ChatHistoryBox />
+
+        {/* Profile dropdown */}
+        <ProfileDropdown />
       </div>
-      {/* Profile dropdown */}
-      <ProfileDropdown />
-    </div>
+    </>
   );
 };
 
