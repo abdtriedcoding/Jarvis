a687024bdd6895a9b94a8985fb70c212487e11f4
sidd-abdullah <siddabdullahofficial@gmail.com>
2024-01-04 17:49:11 +0530

    code login page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,9 +1,22 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+
 const Page = () => {
   return (
-    <>
-      <h1>Login Page</h1>
-      <h3>Lets start building it next.</h3>
-    </>
+    <div className="flex flex-col space-y-6 text-center items-center justify-center h-full">
+      {/* Icon and text */}
+      <div className="flex flex-col space-y-2 text-center items-center">
+        <Avatar className="w-28 h-28">
+          <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+        <h1 className="text-xl font-medium">
+          Authenticate yourself, or stay on the sidelines😉 !
+        </h1>
+      </div>
+      {/* Login BUtton */}
+      <Button size={"lg"}>Login with Google</Button>
+    </div>
   );
 };
 
