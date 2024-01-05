4c21063bbdd4c3d5e759445356fab81cefa38033
sidd-abdullah <siddabdullahofficial@gmail.com>
2024-01-05 19:59:21 +0530

    fix ui bug

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -3,8 +3,10 @@ import ChatBox from "@/components/chatbox";
 
 export default function Home() {
   return (
-    <div className="flex flex-col h-full">
-      <ChatScreen />
+    <div className="flex flex-col h-[80vh]">
+      <div className="flex-1">
+        <ChatScreen />
+      </div>
       <div className="sticky bottom-0">
         <div className="flex flex-col space-y-2 bg-white z-20">
           <ChatBox />
