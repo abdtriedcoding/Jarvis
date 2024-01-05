ffdb42328c4bbab29a9adafc8c1d61ca8e8fac0c
sidd-abdullah <siddabdullahofficial@gmail.com>
2024-01-05 18:41:35 +0530

    protect login page

--- app/(auth)/_components/login-button.tsx
+++ app/(auth)/_components/login-button.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { Button } from "@/components/ui/button";
 import { auth, db } from "@/firebase/firebaseConfig";
 import { doc, getDoc, setDoc } from "firebase/firestore";
--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,7 +1,23 @@
+"use client";
+
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import LoginButton from "../_components/login-button";
+import { auth } from "@/firebase/firebaseConfig";
+import { useRouter } from "next/navigation";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 const Page = () => {
+  const [user, authstateLoading] = useAuthState(auth);
+  const router = useRouter();
+
+  if (authstateLoading) {
+    return null;
+  }
+
+  if (user) {
+    router.push("/");
+  }
+
   return (
     <div className="flex flex-col space-y-6 text-center items-center justify-center h-full">
       <div className="flex flex-col space-y-2 text-center items-center">
