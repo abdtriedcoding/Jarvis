b2d79bdd9ec043b1136b3f207717fa790b8ec4c9
sidd-abdullah <siddabdullahofficial@gmail.com>
2024-01-06 20:44:45 +0530

    refactor auth code

--- app/(auth)/_components/login-button.tsx
+++ app/(auth)/_components/login-button.tsx
@@ -1,59 +1,14 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
-import { auth, db } from "@/firebase/firebaseConfig";
-import { doc, getDoc, setDoc } from "firebase/firestore";
-import { signIn, signOut, useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
-import { SyntheticEvent } from "react";
-import { useSignInWithGoogle } from "react-firebase-hooks/auth";
-import toast from "react-hot-toast";
+import { signIn } from "next-auth/react";
 
 const LoginButton = () => {
-  // const [signInWithGoogle, , loading, error] = useSignInWithGoogle(auth);
-  // const router = useRouter();
-
-  // if (error) {
-  //   toast.error(error.message);
-  // }
-
-  // const handleGoogleSignIn = async (event: SyntheticEvent) => {
-  //   event.preventDefault();
-
-  //   const result = await signInWithGoogle();
-  //   const user = result?.user;
-
-  //   if (user) {
-  //     const docRef = doc(db, `users/${user.uid}`);
-
-  //     // Check if the user data already exists in Firestore
-  //     const docSnap = await getDoc(docRef);
-
-  //     if (!docSnap.exists()) {
-  //       // If the user data doesn't exist, save it in Firestore
-  //       await setDoc(docRef, JSON.parse(JSON.stringify(user)));
-  //     } else {
-  //       // If the user data already exists
-  //       console.log("User data already exists in Firestore");
-  //     }
-  //     router.push("/");
-  //   }
-  // };
-
-  // const { data: session } = useSession();
-  // const session = await getServerSession(authOptions);
-  // const session = useSession();
-  const { data: session, status } = useSession();
-
   return (
     <>
-      {status === "authenticated" ? (
-        <Button onClick={() => signOut()} size="lg">
-          Logout
-        </Button>
-      ) : (
-        <Button onClick={() => signIn("google")} size="lg">
-          Login with Google
-        </Button>
-      )}
+      <Button onClick={() => signIn("google", { callbackUrl: "/" })} size="lg">
+        Login with Google
+      </Button>
     </>
   );
 };
--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,23 +1,7 @@
-"use client";
-
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import LoginButton from "../_components/login-button";
-import { auth } from "@/firebase/firebaseConfig";
-import { useRouter } from "next/navigation";
-import { useAuthState } from "react-firebase-hooks/auth";
 
-const Page = () => {
-  // const [user, authstateLoading] = useAuthState(auth);
-  // const router = useRouter();
-
-  // if (authstateLoading) {
-  //   return null;
-  // }
-
-  // if (user) {
-  //   router.push("/");
-  // }
-
+const Page = async () => {
   return (
     <div className="flex flex-col space-y-6 text-center items-center justify-center h-full">
       <div className="flex flex-col space-y-2 text-center items-center">
