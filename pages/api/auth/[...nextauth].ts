5ee1ca9d8cb7c6598860fa28ff65205cec034c4d
sidd-abdullah <siddabdullahofficial@gmail.com>
2024-01-06 19:19:11 +0530

    successfully auth and storing user in firebase

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,9 @@
-import NextAuth from "next-auth"
+import { FirestoreAdapter } from "@auth/firebase-adapter";
+import { cert } from "firebase-admin/app";
+import NextAuth, { AuthOptions } from "next-auth"
+import { Adapter } from "next-auth/adapters";
 import GoogleProvider from "next-auth/providers/google";
-export const authOptions = {
+export const authOptions: AuthOptions = {
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
@@ -8,5 +11,12 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!
     })
   ],
+  adapter: FirestoreAdapter({
+    credential: cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n'),
+    }),
+  }) as Adapter
 }
 export default NextAuth(authOptions)
