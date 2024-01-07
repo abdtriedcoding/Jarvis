2a47bc86053f613b43133c8d3cf0e99c94f503ed
sidd-abdullah <siddabdullahofficial@gmail.com>
2024-01-07 16:12:08 +0530

    fix props typo

--- app/(main)/chat/[chatId]/page.tsx
+++ app/(main)/chat/[chatId]/page.tsx
@@ -24,7 +24,7 @@ const Page = async ({ params }: { params: { chatId: string } }) => {
   return (
     <div className="flex h-full">
       <aside className="w-64 p-4 hidden md:block bg-slate-50">
-        <Sidebar uid={uid} />
+        <Sidebar />
       </aside>
       <div className="flex-1">
         <div className="h-full flex flex-col">
